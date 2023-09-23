ad7ddbe847c3156d7eafe2033ddc5a0029be265c
Suryakant Walunj <suryakantswalunj@gmail.com>
2023-09-23 16:27:12 +0530

    Updated changes

--- sample1/flutter_application_1/lib/main.dart
+++ sample1/flutter_application_1/lib/main.dart
@@ -2,10 +2,22 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const Center(
-      child: Text(
-        'Hello, world!',
-        textDirection: TextDirection.ltr,
+    Center(
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.red,
+            width: 5.0,
+          ),
+        ),
+        margin: const EdgeInsets.all(10.0),
+        //color: Colors.amber[600],
+        width: 48.0,
+        height: 48.0,
+        child: const Text(
+          'Hello, world!',
+          textAlign: TextAlign.left,
+        ),
       ),
     ),
   );
