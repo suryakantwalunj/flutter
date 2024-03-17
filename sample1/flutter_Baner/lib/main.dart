42529fc4eb196c964c54dd9a9bdf514ac8f59a2c
Suryakant Walunj <suryakantswalunj@gmail.com>
2024-03-17 13:05:32 +0530

    Aligned to material app

--- sample1/flutter_Baner/lib/main.dart
+++ sample1/flutter_Baner/lib/main.dart
@@ -1,25 +1,42 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    Center(
+class MyButton extends StatelessWidget {
+  const MyButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        print('MyButton was tapped!');
+      },
       child: Container(
+        width: 280,
+        height: 48,
+        padding: const EdgeInsets.all(8),
+        transform: Matrix4.rotationZ(0.1),
+        margin: const EdgeInsets.symmetric(horizontal: 8),
         decoration: BoxDecoration(
           border: Border.all(
             color: Colors.red,
             width: 5.0,
           ),
+          borderRadius: BorderRadius.circular(12),
           color: Colors.amber[600],
-          borderRadius: BorderRadius.circular(5.0),
         ),
-        margin: const EdgeInsets.all(10.0),
-        padding: const EdgeInsets.all(5.0),
-        transform: Matrix4.rotationZ(0.1),
-        //width: 96.0,
-        //height: 48.0,
-        child: const Text(
-          'Hello, world, I am surya !',
-          textDirection: TextDirection.ltr,
+        child: const Center(
+          child: Text('I am surya...!'),
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: MyButton(),
         ),
       ),
     ),
